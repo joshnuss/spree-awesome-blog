c4054553afa0020dc86937102307fb7e523fd609
Josh Nussbaum <joshnuss@gmail.com>
2011-01-10 04:09:57 -0500

    added link_to_rss helper

--- lib/spree_simple_blog/engine.rb
+++ lib/spree_simple_blog/engine.rb
@@ -5,7 +5,20 @@ module SpreeSimpleBlog
   class Engine < Rails::Engine
 
     def self.activate
+      Spree::BaseHelper.module_eval do
+        def link_to_rss 
+          tag(:link,
+            :rel => 'alternate',
+            :type => "application/rss+xml", 
+            :title => Spree::Config[:blog_title] || "#{Spree::Config[:site_name]} Blog",
+            :href =>  posts_path(:format => :rss))
+        end
+      end
+      
     end
+
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.to_prepare &method(:activate).to_proc
 
   end
 
